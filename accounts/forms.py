8cda8c2a8f695d687585df97ed61fcf2040c9101
somyasj123 <somyasj123@gmail.com>
2018-07-17 14:48:04 +0530

    First Commit

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserCreateForm(UserCreationForm):
+        username = forms.CharField(max_length=130, required=True , label="Your Username")
+        password1 = forms.CharField(widget=forms.PasswordInput(),label="Your Password")
+        password2 = forms.CharField(widget=forms.PasswordInput(),label="Repeat Your Password")
+        email = forms.EmailField(max_length=130, required=True ,label="Email Address")
+        age = forms.IntegerField(label="Age")
+        first_name = forms.CharField(max_length=130, required=True ,label="Name")
+        last_name = forms.CharField(max_length=130, required=True ,label="Surname")
+        degree = forms.CharField(max_length=130, required=True ,label="Degree name")
+        city = forms.CharField(max_length=130, required=True ,label="City")
+        state = forms.CharField(max_length=130, required=True ,label="State")
+        country = forms.CharField(max_length=130, required=True ,label="Country")
+
+        class Meta:
+            model = User
+            fields = ("first_name", "last_name", "email", "username", "password1", "password2", "degree", "city",
+                      "state", "country", "age")
+
---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import UserCreateForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.userprofile.age = form.cleaned_data.get('age')
+            user.userprofile.degree = form.cleaned_data.get('degree')
+            user.userprofile.city = form.cleaned_data.get('city')
+            user.userprofile.state = form.cleaned_data.get('state')
+            user.userprofile.country = form.cleaned_data.get('country')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreateForm()
+    return render(request, 'accounts/signup.html', {'form': form})
